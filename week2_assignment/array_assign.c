8c7e96d3fd07c5b99f39b0ca385b714a79a7a41e
rabin <uglyzxenun@gmail.com>
2024-09-10 13:22:16 +0300

    delete

--- week2_assignment/array_assign.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-int main(){
-    printf("This is a test to check if the git is working or not");
-    return 0;
-
-}
--- week2_assignment/test.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-int main() {
-    printf("Does it works!!!?");
-
-    return 0;
-}
