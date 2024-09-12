d50b87baefc98b164731e9928fd7e9131fca08ff
rabin <uglyzxenun@gmail.com>
2024-09-12 21:06:39 +0300

    add explain

--- week5_assignment/assignment_3.c
+++ week5_assignment/assignment_3.c
@@ -5,16 +5,16 @@ into array's element. Finally, calculate average value of the all array's elemen
 #include <time.h>
 int main(){                 //main program starts
     double example_a[100];     // initialize array size 100
-    double min_num = 1.0;    // assign a 
-    double max_num = 100.0;
-    srand(time(0));
-    double total_in_array = 0;
-    for(int i = 0; i < 100; i++){
+    double min_num = 1.0;    // initialize and assign minimum value
+    double max_num = 100.0;   // initialize and assign maximum vlaue
+    srand(time(0));     // seed random number generator 
+    double total_in_array = 0;       // initialize and assign total sum in array
+    for(int i = 0; i < 100; i++){     // using for loop to generate random number and add in total
         example_a[i] = ((double) rand() / RAND_MAX) * (max_num - min_num) + min_num;
         printf("%.2lf \n", example_a[i]);
         total_in_array += example_a[i]; 
     }
-    printf(" \nTotal sum in random array is: %.2lf", total_in_array);
+    printf(" \nTotal sum in random array is: %.2lf", total_in_array);  // print total in array
 
 
 
