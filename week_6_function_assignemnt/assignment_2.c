8ab65589f8580c34280b540a4c29855664d10082
rabin <uglyzxenun@gmail.com>
2024-09-13 09:36:35 +0300

    modify comment

--- week_6_function_assignemnt/assignment_2.c
+++ week_6_function_assignemnt/assignment_2.c
@@ -8,8 +8,8 @@ int area_calculator(int r_width, int r_height){   // initialize function to calc
 } 
 
 int main(){
-    int r_width = 9;    // initialize triangle width 
-    int r_height = 6;       // initialize triangle height
+    int r_width = 9;    // initialize rectangle width 
+    int r_height = 6;       // initialize rectangle height
     int area;           // initialize area
     area = area_calculator(r_width, r_height);      // sending and receiving variable and result
     printf("Area of rectangle is: %d  ", area);          // print area
