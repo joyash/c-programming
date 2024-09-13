a3a359f62f34b5c78153211f30576823a003b9ea
rabin <uglyzxenun@gmail.com>
2024-09-13 09:24:29 +0300

    initial commit

---
+++ week_6_function_assignemnt/assignment_1.c
@@ -0,0 +1,18 @@
+/* 1) Make Function func1 to calculate the area of a triangle. Inputs to the function are width 
+and height.  */
+
+#include <stdio.h> // start program
+int area_calculator(int t_width, int t_height){   // initialize function to calculate area 
+    int area = t_width * t_height;   // calculate area 
+    return area;
+} 
+
+int main(){
+    int t_width = 9;    // initialize triangle width 
+    int t_height = 6;       // initialize triangle height
+    int area;           // initialize area
+    area = area_calculator(t_width, t_height);      // sending and receiving variable and result
+    printf("Area of triangle is: %d  ", area);          // print area
+
+    return 0;
+}
