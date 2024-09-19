8ec536504ad63c5b435e3a0bf2bbb5f387419a6e
rabin <uglyzxenun@gmail.com>
2024-09-19 12:38:29 +0300

    scanf modified

--- week_7_file_assignment/assignment_1.c
+++ week_7_file_assignment/assignment_1.c
@@ -4,18 +4,23 @@
  Excel.*/
 
  #include <stdio.h>
+ #include <string.h>
 
  int main(){        // start main  program
     int arra[5];        // initialize array to store value
+    char user_f[10];
+    printf("Choose a name of the file: ");
+    scanf("%s", user_f);
+    strcat(user_f, ".csv");
     for(int i = 0; i < 5; i++){         // for loop to get 5 value from user
         printf("Enter a number: ");     //ask user to input
         scanf("%d", &arra[i]);      //assign those input in variable
 
     }
-    FILE *file = fopen("ass1.csv", "w");        //initialize to write on file
+    FILE *file = fopen(user_f, "w");        //initialize to write on file
     for(int i = 0; i < 5; i++){     //for loop to write value in file
         fprintf(file, "%d", arra[i]);       // write value in file
-        if(i < 5){              
+        if(i < 4){              
             fprintf(file, ","); // put commas after each value
         }
     }fclose(file);
