b117aef4fcd3a61a3894e84565edc3b8d1c7dddb
rabin <uglyzxenun@gmail.com>
2024-09-22 16:38:00 +0300

    write more code

--- final_assignment/code_a1.c
+++ final_assignment/code_a1.c
@@ -1,65 +1,93 @@
-/*NPC and a user play the Rock-Paper-Scissors game.
-Rule:NPC and a user select one in Rock-Paper-Scissors at same timing.
-Rock is stronger than Scissors.
-Paper is stronger than Rock.
-Scissors is stronger than Paper.
-For example, when NPC selects Scissors and a user selects Rock, a user win.
-When selections of NPC and a user is same, no winner and no loser.*/
-// game rule
+/*Assignment 1:
+Five patients arrive at the hospital.
+They input (1) first name, (2) last name, (3) age, (4) gender, (5) height, and (6) weight from a keyboard by scanf.
+Including calculation function of BMI.
+Show averge value of BMI in a display.
+Show the numbers of male/female in a display.
+Show the numbers of person who is over 25 BMI in a display.
+Save (1)-(6), and (7) BMI of all patient to array-type variables.
+Output files on the above arrays, e.g., first_name.csv, lase_name.csv, age.csv ...*/
+
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
- // here
+// function to claculate bmi
+void bmi_cal(float height[], int weight[], float bmi[], int count){
+    for(int i = 0; i < count; i++){
+        bmi[i] = weight[i] / (height[i] * height[i]);
+    }
+}
+
 int main(){
-    int human_player;       // variable for save choice
-    int cpu_player;
-
-    printf("Welcome to Rock, Paper and Scissor Game!!!\n");  // welcome message
-    printf("Player 1 : You\n");
-    printf("Player 2 : Computer\n");
-    int human_win_counter = 0;      // variable for count win record
-    int com_win_counter = 0;
-    int equal = 0;
-
-
-    for(int i = 0; i < 5; i++ ){        // for loop to play game for 5 times 
-        int min_num = 1;                // minimum number for game 
-        int max_num = 3;                // maximum number for game   
-        srand(time(0));                 // seed real time
-        printf("Please select from the options : \nRock = 1\nPaper = 2\nScissor = 3 \n"); // ask user 
-        printf("Enter your choice: ");  // to enter choice to start game 
-        scanf("%d", &human_player);
-                
-        if(human_player >= 1 && human_player <= 3){     // check the user selection if ok game continue
-            cpu_player = rand() % (max_num - min_num + 1) + min_num; // cpu generates its choice
-            printf("Here is computer choice: %d\n", cpu_player);
-
-        if(human_player == 1 && cpu_player == 3 || human_player == 2 && cpu_player == 1 || human_player == 3 && cpu_player == 2){
-            printf("Congratulation you win the game!!!\n"); // if beat computer player wins
-            human_win_counter += 1;   // tracking player wins record
-        }
-        else if(human_player == 3 && cpu_player == 1 || human_player == 1 && cpu_player == 2 || human_player == 2 && cpu_player == 3){
-            printf("Computer wins the game!!!\n");   // if computer beat player computer wins
-            com_win_counter += 1;               // tracking computer wins record
-        }
-        else if(human_player == 2 && cpu_player == 2 || human_player == 1 && cpu_player == 1 || human_player == 3 && cpu_player == 3){
-            printf("Both have same choice! Nobody wins the game!!!\n");  // if both choice same it is tie
-            equal += 1;  // tracking number of ties 
-        }} 
-        else if(human_player < 0 || human_player > 3){ // show error if wrong selection
-            printf("You Select the wrong choice !!!\nSelect the correct choice from the list!!!\n");
-            i--;  // minus counter if wrong choice
-        }
-        else{
-            printf("Pieni Hetki\n Erron on Game!!!\n");
-        }
+    char gender[5], first_name[5][10], last_name[5][10]; // array to store gender, name 
+    int age[5],weight[5];  // array to store age weight
+    float height[5], bmi[5]; // array to store height bmi
+    int male_count = 0;
+    int female_count = 0;
+    float bmi_sum = 0.0;
+    int bmi_over_25 = 0;
+   
+    // open files for writing
+    FILE *file = fopen("first_name.csv", "a");
+    FILE *file1 = fopen("last_name.csv", "a");
+    FILE *file2 = fopen("gender.csv", "a");
+    FILE *file3 = fopen("age.csv", "a");
+    FILE *file4 = fopen("height.csv", "a");
+    FILE *file5 = fopen("weight.csv", "a");
+    FILE *file6 = fopen("bmi.csv", "a");
+    
+    // using loop to collect data for 5
+    for(int i = 0; i < 5; i++){
+            printf("Enter your first name: ");
+            scanf("%s", first_name[i]);
+            printf("Enter your last name: ");
+            scanf("%s", last_name[i]);
+            printf("Enter your gender: ");
+            scanf(" %c", &gender[i]);
+            printf("Enter your age: ");
+            scanf("%d", &age[i]);
+            printf("Enter your height: ");
+            scanf("%f", &height[i]);
+            printf("Enter your weight: \n");
+            scanf("%d", &weight[i]);
+    
+        // write data to files
+        fprintf(file, "%s\n", first_name[i]);
+        fprintf(file1, "%s\n", last_name[i]);
+        fprintf(file2, "%c\n", gender[i]);
+        fprintf(file3, "%d\n", age[i]);
+        fprintf(file4, "%.2lf\n", height[i]);
+        fprintf(file5, "%d\n", weight[i]);
+        
+     
+     // keeping track of male and female number
+     if(gender[i] == 'm' || gender[i] == 'M'){
+         male_count +=1 ;}
+    else if(gender[i] == 'f' || gender[i] == 'F'){
+        female_count += 1;
     } 
-    printf("Here is the final result !!!\n");      // print final result
-    printf("You win %d times !!!\n", human_win_counter);
-    printf("Computer wins %d times !!!\n" , com_win_counter);
-    printf("%d times it was tie!!!", equal);
-
-
+    }
+    // calculate bmi for each 
+    bmi_cal(height, weight, bmi, 5);
+    for(int i = 0; i < 5; i++){
+        fprintf(file6, "%.2lf\n", bmi[i]);
+        bmi_sum += bmi[i];
+        if(bmi[i] > 25){
+            bmi_over_25 ++;
+        }
+        
+        
+    }
+    // display results
+    printf("Average BMI: %.lf\n", bmi_sum / 5);
+    printf("Number of males: %d\n", male_count);
+    printf("Number of females: %d\n", female_count);
+    printf("Number of people with over BMI 25 : %d\n", bmi_over_25);
+    fclose(file);
+    fclose(file1);
+    fclose(file2);
+    fclose(file3);
+    fclose(file4);
+    fclose(file5);
+    fclose(file6);
+    
     return 0;
-
 }
